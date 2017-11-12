78dcf4f906e44d0778c5715fbf53cc37567d1905
Qing Cheng <oncevincentcheng@gmail.com>
2017-11-12 22:51:48 +0100

    w2 update

--- Courses/ComputerVision/Code/MatchedSignalDetetor.m
+++ Courses/ComputerVision/Code/MatchedSignalDetetor.m
@@ -1,16 +1,5 @@
 % Find template 1D
 % NOTE: Function definition must be the very first piece of code here!
-function index = find_template_1D(t, s)
-    % TODO: Locate template t in signal s and return index
-    % NOTE: Turn off all output from inside the function before submitting!
-    s_size = size(s, 2);
-    t_size = size(t, 2);
-    mul = zeros(1,s_size - t_size + 1);
-    
-    for i = 1: s_size - t_size + 1
-	 
-endfunction
-
 pkg load image; % AFTER function definition
 
 % Test code:
@@ -19,5 +8,19 @@ t = [1 1 0];
 disp('Signal:'), disp([1:size(s, 2); s]);
 disp('Template:'), disp([1:size(t, 2); t]);
 
-index = find_template_1D(t, s);
+t(t==0) = -1;
+s_size = size(s, 2);
+t_size = size(t, 2);
+mul = zeros(1,s_size - t_size + 1);
+t1 = [t, mul(1:(size(mul,2)-1))];
+disp(t1);
+
+for i = 1: s_size - t_size + 1
+  mul(i) = t1 * s';
+  t1 = circshift(t1,1,2);
+endfor
+
+disp('mul:'), disp([1:size(mul, 2); mul]);
+[val,index] = max(mul);
+
 disp('Index:'), disp(index);
